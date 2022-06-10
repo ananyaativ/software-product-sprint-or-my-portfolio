58f89927b095f3551c4adc3ab561ea4fedee26ff
ananyaativ <84693150+ananyaativ@users.noreply.github.com>
2022-06-10 09:06:58 +0530

    Changed the complete portfolio as compared to week 1 (#3)
    * Update the README.md file to describe the repo.
    
    * Add images folder and modify pom and index
    
    * add random fact and quote
    
    * Add LinkedIn and Github Porfiles
    
    * Add paragraphs about myself
    
    * Add photo gallery
    
    * Add Projects and Hidden Talent
    
    * format index
    
    * Add styling
    
    * format file
    
    * Add tempelate
    
    * Add header, skills, and education
    
    * Add project section
    
    * Add project images
    
    * Add work experience
    
    * Add hidden talent and about me
    
    * Change button style
    
    * Add correct jquery plugin
    
    * Add hard coded string to servlet
    
    * Use fetch to get server content
    
    * Add js code

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -5,6 +5,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.gson.Gson;
+import java.util.ArrayList;
 
 /** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
 @WebServlet("/hello")
@@ -12,7 +14,17 @@ public class HelloWorldServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
+    ArrayList<String> facts=new ArrayList<>();
+    facts.add("I have never lost a game of Othello.");
+    facts.add("I was born in Delhi, India.");
+    facts.add("My second toe is bigger than my first toe.");
+    facts.add("I can touch my nose with my tongue.");
+    Gson gson = new Gson();
+    String json = gson.toJson(facts);
+
+    // Send the JSON as the response
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
+
   }
 }
